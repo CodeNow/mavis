22fee4a6767628d650d3dd58b7b52483a790667c
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-07-16 21:23:56 -0700

    update return object for docks

--- middleware.js
+++ middleware.js
@@ -3,12 +3,13 @@ var mavis = require('./mavis.js');
 
 module.exports = function process (req, res, next, cb) {
   var hint = req.body;
-
   mavis.obtainOptimalHost(hint , function (err, dockHost) {
     cb();
     if (err) {
       return next(err);
     }
-    res.send(200, dockHost);
+    res.json(200, {
+      dockHost: dockHost
+    });
   });
 };
