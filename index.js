99c0a68951776def5c6cbf383ecf1880171248fa
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-01-20 13:36:32 -0800

    add new relic and datadog

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 require('./lib/loadenv.js')();
+require('newrelic');
+var dd = require('./lib/models/datadog.js');
+dd.monitorStart();
 var app = require('./lib/app.js');
 
 app.listen(process.env.PORT);
