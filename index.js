a4c85b03ea9be5de1b909391380fd5bbc7848bcb
Ryan Sandor Richards <ryan@runnable.com>
2015-03-12 18:05:20 -0700

    Minor tweaks for easier development.
    - Server now reports it is running (and on what port).
    - Newrelic is skipped if you set `BYPASS_NEWRELIC=true` in the process environment.
    - Newrelic explicitly skipped for `development` and `test` enviornments

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict';
 require('./lib/loadenv.js')();
-require('newrelic');
+
+if (process.env.BYPASS_NEWRELIC != 'true') {
+  require('newrelic');
+}
+
 var dd = require('./lib/models/datadog.js');
 dd.monitorStart();
-var app = require('./lib/app.js');
 
+var app = require('./lib/app.js');
 app.listen(process.env.PORT);
+console.log('mavis started and running on port ' + process.env.PORT);
