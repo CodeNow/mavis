b541c131c1a22a9391975d619ebd9bfc690df202
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-09-02 16:37:05 -0700

    add new cluster-instance-provision job when unhealthy

--- test/functional/workers/on-dock-unhealthy.js
+++ test/functional/workers/on-dock-unhealthy.js
@@ -11,6 +11,8 @@ var beforeEach = lab.beforeEach;
 var Code = require('code');
 var expect = Code.expect;
 
+var createCount = require('callback-count');
+
 var dockData = require('../../../lib/models/dockData.js');
 var server = require('../../../lib/server.js');
 var rabbitMQ = require('../../../lib/rabbitmq.js');
@@ -18,30 +20,45 @@ var redisClient = require('../../../lib/models/redis.js');
 
 describe('on-dock-unhealthy functional test', function () {
   var testHost = 'http://10.20.1.26:4242';
+  var testGihubId = 2194285;
+
   before(function(done) {
     server.start(done);
   });
+
   lab.beforeEach(function (done) {
     redisClient.flushall(done);
   });
+
   beforeEach(function(done) {
     dockData.addHost(testHost, 'test,tags', done);
   });
+
   after(function(done) {
     server.stop(done);
   });
 
   lab.experiment('on-docker-unhealthy event', function () {
-    lab.test('should remove host and public on-dock-removed', function (done) {
-      rabbitMQ.hermesClient.publish('on-dock-unhealthy', { host: testHost });
+    lab.test('should remove host and publish two events', function (done) {
+      var count = createCount(2, done);
+
+      rabbitMQ.hermesClient.subscribe('cluster-instance-provision', function (data, cb) {
+        expect(data.githubId).to.equal(testGihubId);
+        cb();
+        count.next();
+      });
       rabbitMQ.hermesClient.subscribe('on-dock-removed', function (data, cb) {
         expect(data.host).to.equal(testHost);
         cb();
         dockData.getAllDocks(function (err, data) {
-          if (err) { return done(err); }
+          if (err) { return count.next(err); }
           expect(data.length).to.equal(0);
-          done();
+          count.next();
         });
+      });
+      rabbitMQ.hermesClient.publish('on-dock-unhealthy', {
+        host: testHost,
+        githubId: testGihubId
       });
     });
   }); // on-docker-unhealthy event
