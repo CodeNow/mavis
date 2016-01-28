3c72c29ec68838b50e817a997720ede193b2de4b
Anton Podviaznikov <podviaznikov@gmail.com>
2016-01-28 13:29:43 -0800

    remove github id validation

--- test/unit/workers/on-dock-unhealthy.js
+++ test/unit/workers/on-dock-unhealthy.js
@@ -25,14 +25,18 @@ var onDockUnhealthy = require('../../../lib/workers/on-dock-unhealthy.js');
 describe('lib/workers/on-dock-unhealthy unit test', function () {
   describe('failed', function () {
     beforeEach(function (done) {
-      sinon.spy(Events, '_hasValidHost');
+      sinon.spy(Events, '_hasValidHost')
       sinon.stub(dockData, 'deleteHost')
+      sinon.stub(dockData, 'getDockByHost').yieldsAsync(null, {
+        tags: '12312,build,run'
+      })
       done();
     });
 
     afterEach(function (done) {
       Events._hasValidHost.restore();
       dockData.deleteHost.restore();
+      dockData.getDockByHost.restore();
       done();
     });
 
@@ -45,15 +49,58 @@ describe('lib/workers/on-dock-unhealthy unit test', function () {
         });
     });
 
+    it('should throw error get host failed', function (done) {
+      dockData.getDockByHost.yieldsAsync(new Error('Redis error'));
+      onDockUnhealthy({
+        host: 'http://10.12.12.11:4242'
+      })
+      .catch(function (err) {
+        expect(err).to.be.instanceOf(TaskFatalError);
+        expect(err.message).to.contain('Dock is not in mavis');
+        sinon.assert.calledOnce(Events._hasValidHost);
+        sinon.assert.calledOnce(dockData.getDockByHost);
+        sinon.assert.calledWith(
+          dockData.getDockByHost,
+          'http://10.12.12.11:4242'
+        );
+        sinon.assert.notCalled(dockData.deleteHost);
+        done();
+      });
+    });
+
+    it('should throw error get host found no host', function (done) {
+      dockData.getDockByHost.yieldsAsync(null);
+      onDockUnhealthy({
+        host: 'http://10.12.12.11:4242'
+      })
+      .catch(function (err) {
+        expect(err).to.be.instanceOf(TaskFatalError);
+        expect(err.message).to.contain('Dock is not in mavis');
+        sinon.assert.calledOnce(Events._hasValidHost);
+        sinon.assert.calledOnce(dockData.getDockByHost);
+        sinon.assert.calledWith(
+          dockData.getDockByHost,
+          'http://10.12.12.11:4242'
+        );
+        sinon.assert.notCalled(dockData.deleteHost);
+        done();
+      });
+    });
+
     it('should throw error delete host failed', function (done) {
       dockData.deleteHost.yieldsAsync(new Error('Redis error'));
       onDockUnhealthy({
-        host: 'http://10.12.12.11:4242',
-        githubId: '2335750'
+        host: 'http://10.12.12.11:4242'
       })
       .catch(function (err) {
         expect(err).to.be.instanceOf(TaskError);
+        expect(err.message).to.contain('Failed to delete host');
         sinon.assert.calledOnce(Events._hasValidHost);
+        sinon.assert.calledOnce(dockData.getDockByHost);
+        sinon.assert.calledWith(
+          dockData.getDockByHost,
+          'http://10.12.12.11:4242'
+        );
         sinon.assert.calledOnce(dockData.deleteHost);
         sinon.assert.calledWith(
           dockData.deleteHost,
@@ -68,14 +115,18 @@ describe('lib/workers/on-dock-unhealthy unit test', function () {
     beforeEach(function (done) {
       sinon.spy(Events, '_hasValidHost');
       sinon.stub(dockData, 'deleteHost').yieldsAsync(null);
+      sinon.stub(dockData, 'getDockByHost').yieldsAsync(null, {
+        tags: '12312,build,run'
+      });
       done();
     });
 
     afterEach(function (done) {
-      Events._hasValidHost.restore();
-      dockData.deleteHost.restore();
-      rabbitMQ._publisher = null;
-      done();
+      Events._hasValidHost.restore()
+      dockData.deleteHost.restore()
+      dockData.getDockByHost.restore()
+      rabbitMQ._publisher = null
+      done()
     });
 
     it('should emit provision and wait events', function (done) {
@@ -86,11 +137,16 @@ describe('lib/workers/on-dock-unhealthy unit test', function () {
       };
       sinon.spy(rabbitMQ._publisher, 'publish');
       onDockUnhealthy({
-        host: dockerUrl,
-        githubId: 12312
+        host: dockerUrl
       })
       .then(function () {
         sinon.assert.calledOnce(Events._hasValidHost);
+
+        sinon.assert.calledOnce(dockData.getDockByHost);
+        sinon.assert.calledWith(
+          dockData.getDockByHost,
+          dockerUrl
+        )
 
         sinon.assert.calledOnce(dockData.deleteHost);
         sinon.assert.calledWith(
